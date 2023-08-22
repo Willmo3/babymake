a3d079a712bb06c1b5d9a1cf556223c5d5a5f1d0
Willmo3 <thelordofbouge@gmail.com>
2023-08-22 14:09:49 -0400

    Switch bm.py to just use filename.

--- bm.py
+++ bm.py
@@ -4,16 +4,15 @@ from sys import argv
 from os import getcwd
 from os.path import isfile
 
-ARG_LEN = 3
-LANG_INDEX = 1
-TARGET_INDEX = 2
+ARG_LEN = 2
+FILE_INDEX = 1
 
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1 
 
 def main():
     if len(argv) != ARG_LEN:
-        print("Usage: python bm.py [language] [name]")
+        print("Usage: python bm.py [filename]")
         return EXIT_FAILURE
 
     comp = None           # Compiler to use.
@@ -21,23 +20,21 @@ def main():
     ext = None            # extension for source file.
     target = None         # Name of output file.
 
-    lang = argv[LANG_INDEX]
-    if lang.lower() == "c":
-        comp = "gcc"
-        ext = ".c"
-    elif lang.lower() == "cpp":
-        comp = "g++"
-        ext = ".cpp"
-    else:
-        print("Invalid language: " + lang + ". Options are: c, cpp")
-        return EXIT_FAILURE
-
-    target = argv[TARGET_INDEX]
-    src = "{}{}".format(argv[TARGET_INDEX], ext)
-
+    src = argv[FILE_INDEX]
     if not isfile(src):
         print("Invalid source file (does it exist?): " + src)
         return EXIT_FAILURE
+
+    ext = src[src.find('.'):]
+    if ext == ".c":
+        comp = "gcc"
+    elif ext == ".cpp":
+        comp = "g++"
+    else:
+        print("Invalid extension: " + ext + ". Options are: .c, .cpp")
+        return EXIT_FAILURE
+
+    target = src[:src.find('.')]
     
     path = getcwd() + "/makefile"
     # Prevent accidental deletions. 
